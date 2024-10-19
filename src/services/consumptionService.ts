82ec7beb5dfa4c7aaa7e9a8832bb3a96e6b083b8
SiegfredLorelle <siegfredlorellecmina@iskolarngbayan.pup.edu.ph>
2024-10-20 03:57:17 +0800

    refactor: use getTotalConsumption for monthly

--- src/services/consumptionService.ts
+++ src/services/consumptionService.ts
@@ -82,38 +82,18 @@ export const getTotalConsumptionThisMonth = async () => {
   const endOfMonth = new Date(currentYear, currentMonth+1, 0, 23, 59, 59, 999);
 
   let userCredential = getUserCredential();
-  if (!userCredential) {
-    return {data: null, error: "Error has occurred."};
+  if (!userCredential || !userCredential.uid) {
+    return errorResponse(913, "Can't identify user.");
   }
-  try {
-    // Query for actual data, do NOT delete this
-    const consumptionCollection = firestore().collection('consumptions');
-    const ConsumptionThisMonthQuery = await consumptionCollection
-      .where('userID', '==', userCredential.uid)
-      .where('recordedAt', '>=', startOfMonth)
-      .where('recordedAt', '<=', endOfMonth)
-      .get();
-  
-    let totalConsumption = 0;
-    let mostRecentReading = startOfMonth;
-    ConsumptionThisMonthQuery.forEach(doc => {
-      const data = doc.data();
-      totalConsumption+= data.kilowattHour;
-      const recordedAt = data.recordedAt.toDate();
-      mostRecentReading = mostRecentReading > recordedAt
-        ? mostRecentReading
-        : recordedAt;
-    });
-    const data = {
-      mostRecentReading: mostRecentReading,
-      totalConsumption: totalConsumption,
-    };
-    // console.log(data);
-    return {data: data, error: ''};
+  let params = {
+    userID: String(userCredential.uid),
+    dateLowerBound: startOfMonth,
+    dateUpperBound: endOfMonth,
   }
-  catch {
-    return {data: null, error: "Error has occurred."};
-  }
+
+  console.log(params);
+  const response = await getTotalConsumption(params);
+  return response;
 };
 
 export const getTotalConsumptionThisYear = async () => {
